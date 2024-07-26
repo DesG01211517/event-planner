6dd4ddc5cfbc0073d0b064f22ed889fe1fb6f688
DesG01211517 <desmongarrison2@gmail.com>
2024-07-26 13:57:13 -0400

    modified eventlist and eventform jsx

--- src/app/management/pages.jsx
+++ src/app/management/pages.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import { EventForm } from "../components/EventForm.js";
+import { EventList } from "../components/EventList.js"
+
+
+
+
+
+
+
 
 export default function Management Page() {
   return (
     <div>Management Page</div>
   )
 }
+
+<EventForm />
